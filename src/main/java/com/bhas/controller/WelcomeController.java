894b1ccf443120f2fc5cc9cc65e852e2d25ed385
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-09 21:43:14 +0530

    @Slf4j

--- src/main/java/com/bhas/controller/WelcomeController.java
+++ src/main/java/com/bhas/controller/WelcomeController.java
@@ -4,13 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/welcome")
+@Slf4j
 public class WelcomeController 
 {
 	@GetMapping("/welcome")
 	public String wishMsg()
 	{
+		log.info("@Slf4j");
 		return "Good Morning,";
 	}
 }
