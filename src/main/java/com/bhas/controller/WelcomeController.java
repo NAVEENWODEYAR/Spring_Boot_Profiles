433e78ace55db062979a62a472c35e14d3959698
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-21 22:31:34 +0530

    @ResponseStatus(code =  HttpStatus.FOUND)

--- src/main/java/com/bhas/controller/WelcomeController.java
+++ src/main/java/com/bhas/controller/WelcomeController.java
@@ -1,7 +1,9 @@
 package com.bhas.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 public class WelcomeController 
 {
 	@GetMapping("/sp")
+	@ResponseStatus(code =  HttpStatus.FOUND)
 	public String wishMsg()
 	{
 		log.info("@Slf4j");
